9adfb3741d3f12c311b75b11049bd91e287232b2
Bruno-SHEMA <b.shema3@alustudent.com>
2026-06-27 13:13:15 -0700

    Quest14 logical gatekeeper using and operator

---
+++ quests/quest_14_logical_gatekeeper.py
@@ -0,0 +1,4 @@
+age =  int(input("Enter your age: "))
+gold = int(input("Enter gold coins you have: "))
+if age >=18 and gold >= 20: print ("You are old enough and have enough coins, you can enter")
+else: print("You don't have enough requirements to enter")
