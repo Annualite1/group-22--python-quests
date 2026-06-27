8a4a9f8129ff9e959fa37052ffbd8cea1d98355a
Annualite1 <a.muhimpund@alustudent.com>
2026-06-27 07:33:43 -0700

    Complete Quest 4: The Town Crier

---
+++ quests/quest_04_town_crier.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+city_name = "Kigali"
+current_year = 2026
+your_name = "Annualite"
+
+print(f"Welcome to {city_name}! The year is {current_year}, and our newest resident is {your_name}.")
