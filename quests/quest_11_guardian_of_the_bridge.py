65ae33fcfc904bb465b37b53d8f9e78855aa3643
Bruno-SHEMA <b.shema3@alustudent.com>
2026-06-27 11:03:25 -0700

    Debbugging quest 11 error

--- quests/quest_11_guardian_of_the_bridge.py
+++ quests/quest_11_guardian_of_the_bridge.py
@@ -1,3 +1,3 @@
-age = int(input("Enter your age"))
-if age >= 18 : print("You are old enough to vote")
-else : print("You are not old enogh to vote")
+age = int(input("Enter your age: "))
+if age >= 18 : print("You are old enough to vot!e")
+else : print("You are not old enogh to vote!")
