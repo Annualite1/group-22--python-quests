ca71b45329089c089b30e6bfba87380ce039f3b6
bruno_alain_shema <b.shema3@alustudent.com>
2026-06-29 16:02:25 +0300

    Commenting quest_12

--- quests/quest_12_two_path_cave.py
+++ quests/quest_12_two_path_cave.py
@@ -1,5 +1,8 @@
+# Initiated a variable called 'password' to store the password
 password = "12345"
+# Initiated a new variable to store a user-inputted password
 user_password = input("Enter password to check: ")
+# conditional statement to check if the password we have matches the one the user inserted, if it is correct, grant access if its not deny
 if password == user_password:
 	print ("Access Granted")
 else: print("Acess Denied")
