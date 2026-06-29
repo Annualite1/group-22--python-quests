ed6fe8a0451e3a302002f53e6c9bf6b9314fc0d3
bruno_alain_shema <b.shema3@alustudent.com>
2026-06-29 15:59:21 +0300

    Adding comments to quest_11

--- quests/quest_11_guardian_of_the_bridge.py
+++ quests/quest_11_guardian_of_the_bridge.py
@@ -1,2 +1,4 @@
+# Variable called age to store the age value entered by the user through input
 age = int(input("Enter your age: "))
+# if conditional statement to check if the user age is greater than 18
 if age >= 18 : print("You are old enough to vot!e")
