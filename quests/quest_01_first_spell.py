4fd8f638df9822528e4809375269d792c2b96989
Annualite1 <a.muhimpund@alustudent.com>
2026-06-29 05:42:28 -0700

    Restore quests 1-5 deleted by teammate

---
+++ quests/quest_01_first_spell.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+
+print("Welcome, adventurer! Your journey begins now!")
---
+++ quests/quest_02_naming_ceremony.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+hero_name = "Fred Gisa Rwigema"
+superpower = "Leadership"
+
+print(f"{hero_name} is remembered for his {superpower}.")
